2cf447abd953ffbe02b7db5cd977ff777bd357a8
rhe18 <rhe18@bu.edu>
2017-12-09 16:00:49 -0500

    V31
    V31

--- lib/ball.py
+++ lib/ball.py
@@ -34,6 +34,7 @@ class Ball(sprobj.SprObj):
 	def check_collision_sides(self, window_width):
 		if(self.left < 1):
 			print "Left collision detected"
+			return True
 		elif(self.right > window_width - 1):
 			print "Right collision detected"
-		return True
+			return True
