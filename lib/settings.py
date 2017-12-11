6d8ee6f4f2135aca53b2f14d4fdf7a1f6d26e75c
bdhkim <31892654+bdhkim@users.noreply.github.com>
2017-12-11 12:12:52 -0500

    Comments Added
    explanation for functions

--- lib/settings.py
+++ lib/settings.py
@@ -1,9 +1,12 @@
+#configure IP of the hosting server
 SERVER_IP = "155.41.82.139"
 SERVER_PORT = 50090
 
+#pyglet window size
 WINDOW_WIDTH = 697
 WINDOW_HEIGHT = 353
 
+#the move speed defines the racket here, not the ball
 MOVE_SPEED = 12
 BALL_IMG = 'assets/ball.png'
 RACKET_IMG = 'assets/racket.png'
